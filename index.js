0f476894016dba86907342ae35c824ee8c68c7f6
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2021-12-01 17:37:37 -0800

    fixing method

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const userRoute = require('./routes/users')
 const movieRoute = require('./routes/movies')
 const listRoute = require('./routes/lists')
 const cors = require('cors')
+const path = require("path");
 
 
 dotenv.config()
@@ -32,6 +33,21 @@ app.use(
     })
 )
 
+
+
+
+app.use(express.static(path.resolve(__dirname, "./netflix/build")));
+
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./netflix/build", "index.html"));
+});
+
+
+
+
+
+
+
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 app.use('/api/movies', movieRoute)
@@ -41,12 +57,13 @@ app.use('/api/lists', listRoute)
 
 
 
-  
-app.use(express.static(path.join(__dirname, "./netflix/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/netflix/build', 'index.html'));
-});
+  
+// app.use(express.static(path.join(__dirname, "./netflix/build")));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/netflix/build', 'index.html'));
+// });
 
 
 
